44d09123a327af893fe30a57fb3d654b4ab1cf8c
Maciej Lewicki <lewickima@drexel.edu>
2020-06-18 09:00:21 -0400

    create blinker for tabeditor

--- src/Components/TabEditor.js
+++ src/Components/TabEditor.js
@@ -2,19 +2,41 @@
 import React from 'react';
 import BaseComponent from './BaseComponent.js';
 
-function fillTab(length, stringNum, position) {
-    if(position[0] === stringNum) {
-        return '-'.repeat(position[1]) + '|' + '-'.repeat(length - position[1]); 
+function fillTab(length, map) {
+    let ret = "";
+    let symbols = map.map(val => val.symbol);
+    let indices = map.map(val => val.position);
+    let position = 0;
+
+    for(var i = 0; i < length; ++i) {
+        if(i === indices[position]) {
+            ret += symbols[position];
+            position += 1;
+        } else ret += '-';
     }
 
-    return '-'.repeat(length + 1);
+    return ret;
+}
+
+function clearTabBlinker(map, cursor) {
+    return map.filter(val => val.symbol !== cursor);
 }
 
 export default class TabEditor extends BaseComponent {
     blinkerFunction() {
+        let stringMap = this.state.map[this.state.position[0]];
+        if(stringMap.find(val => val.symbol === this.state.cursor)) {
+            stringMap = clearTabBlinker(stringMap, this.state.cursor);
+        } else {
+            stringMap.push({position: this.state.position[1], symbol: this.state.cursor});
+            stringMap.sort((f, o) => f.position < o.position ? -1 : 1)
+        }
+
+        let map = this.state.map;
+        map[this.state.position[0]] = stringMap;
+
         this.update({
-            oldString: this.state.position[0],
-            position: [this.state.oldString, this.state.position[1]]    
+            map: map
         })
     }
 
@@ -22,21 +44,26 @@ export default class TabEditor extends BaseComponent {
         super(props);
         this.blinkerFunction = this.blinkerFunction.bind(this);
 
+        let map = [];
+        for(var i = 0; i < this.props.strings.length; ++i) {
+            map.push([]);
+        }
+
         this.state = {
             position: [0, 0],
             length: props.length || 10,
-            oldString: -1,
+            cursor: props.cursor || '|',
+            map: map
+        };
+    }
+
+    componentDidMount() {
+        this.update({
             blinker: setInterval(this.blinkerFunction, this.props.interval || 500)
-        }
+        })
     }
 
     changePosition(mod) {
-        clearInterval(this.state.blinker);
-
-        if(this.state.position[0] === -1) {
-            this.state.position[0] = this.state.oldString;
-        }
-
         let position = [this.state.position[0] + mod[0], this.state.position[1] + mod[1]]
         let length = this.state.length;
 
@@ -45,11 +72,15 @@ export default class TabEditor extends BaseComponent {
         if(position[0] > this.props.strings.length) position[0] = this.props.strings.length;
         if(position[1] > this.state.length) length += 1;
 
+        clearInterval(this.state.blinker);
+        let map = this.state.map;
+        map[position[0]] = clearTabBlinker(map[position[0]], this.state.cursor);
+
         this.update({
             position: position,
-            oldString: -1,
             length: length,
-            blinker: setInterval(this.blinkerFunction, this.props.interval || 500)
+            blinker: setInterval(this.blinkerFunction, this.props.interval || 500),
+            map: map
         });
     }
 
@@ -57,7 +88,7 @@ export default class TabEditor extends BaseComponent {
         return (
             <React.Fragment>
                 <ul className="string-tab">
-                    {this.props.strings.map((val, index) => <li key={val}>{val} |{fillTab(this.state.length, index, this.state.position)}</li>)}
+                    {this.props.strings.map((val, index) => <li key={val}>{val} |{fillTab(this.state.length, this.state.map[index])}</li>)}
                 </ul>
                 <button onClick={() => this.changePosition([0, -1])}>&lt;=</button>
                 <button onClick={() => this.changePosition([-1, 0])}>^^</button>
