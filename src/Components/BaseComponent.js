e8c26a34c0c02cc74df787170fa660f4d57862bb
Maciej Lewicki <lewickimapl@gmail.com>
2020-11-07 21:46:31 +0100

    set up basecomponent to allow hooking in to state changes

--- src/Components/BaseComponent.js
+++ src/Components/BaseComponent.js
@@ -2,8 +2,22 @@
 import React from 'react';
 
 export default class BaseComponent extends React.Component {
+    hooks = {}
+
     update(state) {
         this.props.onChange && this.props.onChange(state);
         this.setState(state);
+
+        Object.keys(state).forEach(val => {
+            if(this.hooks[val]) {
+                this.hooks[val](state);
+            }
+        })
+    }
+
+    addHook(keys, func) {
+        if(Array.isArray(keys)) {
+            keys.forEach(val => this.hooks[val] = func);
+        } else this.hooks[keys] = func;
     }
 }
